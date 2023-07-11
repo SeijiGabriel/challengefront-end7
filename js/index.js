4705395fd3bf2a67b44e330865448a3e6602501c
Gabriel Kanda <gabrielkanda@MacBook-Air-de-Gabriel.local>
2023-07-11 17:38:59 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    // Effect
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
